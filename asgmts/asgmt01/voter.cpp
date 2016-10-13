e256aacfb3f4c45b7db95c854c19c8c352d4e52f
PatrickRoanhouse <Patrick.M.Roanhouse@gmail.com>
2016-10-13 12:49:19 -0700

    its a start

--- asgmts/asgmt01/voter.cpp
+++ asgmts/asgmt01/voter.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <string.h>
+#include <iomanip>
 #include "voter.h"
 
 using namespace std;
 
+
 Voter::Voter(const char * const name, const Party party, const int intensity)
 {
 	// your code here, or in this constructor's initialization list
+	this->Party(party);
+	this->intensity(intensity);
+
+	this->name = new char[strlen(name)+1];
 
 }
 
@@ -20,11 +26,13 @@ void Voter::displayColumnHeadings(ostream& out)
 {
 	// your code here
 
+
 }
 
 ostream& operator<<(ostream& out, Voter* voter)
 {
 	// your code here
+
 	
 
 	return out;
