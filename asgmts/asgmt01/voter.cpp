e0a87cdd501c2f2272b45e46dfdb26a9b48e8bba
PatrickRoanhouse <Patrick.M.Roanhouse@gmail.com>
2016-10-18 12:44:00 -0700

    step 17 fail win

--- asgmts/asgmt01/voter.cpp
+++ asgmts/asgmt01/voter.cpp
@@ -7,10 +7,11 @@ using namespace std;
 
 
 Voter::Voter(const char * const name, const Party party, const int intensity)
+	party{party},
+	intensity{intensity}
 {
 	// your code here, or in this constructor's initialization list
-	party{party};
-	intensity{intensity};
+
 
 	this->name = new char[strlen(name)+1];
 	strcpy(this->name, name);
@@ -21,6 +22,7 @@ Voter::~Voter()
 {
 	// your code here
 	delete [] name;
+	name = nullptr;
 
 
 }
@@ -33,26 +35,34 @@ void Voter::displayColumnHeadings(ostream& out)
 
 
 }
-
 ostream& operator<<(ostream& out, Voter* voter)
+{
+	out << setw(26) << left << voter->name << setw(17) << voter->party << setw(9) << right << voter->intensity << endl;
+
+	return out;
+
+}
+
+
+ostream& operator<<(ostream& out, Party party)
 {
 	// your code here
 	switch(party)
 	{
 		case Party::Republican:
-			cout << "Republican";
+			out << "Republican";
 			break;
 		case Party::Democrat:
-			cout << "Democrat";
+			out << "Democrat";
 			break;
 		case Party::Libertarian:
-			cout << "Libertarian";
+			out << "Libertarian";
 			break;
 		case Party::Green:
-			cout << "Green";
+			out << "Green";
 			break;
 		default:
-			cout << "Not Recognized";		
+			out << "Not Recognized";		
 
 	}
 	
