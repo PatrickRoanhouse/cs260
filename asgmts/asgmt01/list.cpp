803c4bf073c20370aceb69d2eec1db4016c38a0e
PatrickRoanhouse <Patrick.M.Roanhouse@gmail.com>
2016-10-18 12:03:27 -0700

    step 14 fail win

--- asgmts/asgmt01/list.cpp
+++ asgmts/asgmt01/list.cpp
@@ -8,8 +8,8 @@ const char	List::YOUR_NAME[]{"Patrick Roanhouse"};
 List::List()
 {
 	// your code here, or in this constructor's initialization list
-	*headByName = NULL;
-	*headByIntensity = NULL;
+	headByName = NULL;
+	headByIntensity = NULL;
 
 	
 }
@@ -17,6 +17,11 @@ List::List()
 List::~List()
 {
 	// your code here
+		Node * curr(nullptr);
+		Node * prev (nullptr);
+		curr = headByName;
+		while(curr != nullptr)
+
 
 
 }
@@ -27,9 +32,9 @@ List::Node::Node(const Voter& voter)
 	// your initialization list here
 {
 	// your code here
-	 item.name = voter.name;
-	 item.party = voter.party;
-	 item.intensity = voter.intensity;
+	item{voter.getName(), voter.getParty(), voter.getIntensity()},
+	nextByName(nullptr);
+	nextByIntensity(nullptr);
 
 }
 
@@ -37,60 +42,143 @@ List::Node::Node(const Voter& voter)
 void List::displayByName(ostream& out) const
 {
 	// your code here
+	node * curr{nullptr};
+
+	Voter::displayColumnHeadings(out);
+
+	if(headByName == nullptr)
+	{
+		out << "List is empty" << endl;
+	}
+	else if(headByName != nullptr)
+	{
+		curr = headByName;
+		while(curr != nullptr)
+		{
+			out << &curr->item;
+			curr = curr->nextByName;
+		}
+	}
 
 }
 
 void List::displayByIntensity(ostream& out) const
 {
 	// your code here
+	Node * curr{nullptr}
+	Voter::displayColumnheadings(out);
+	if(headByIntensity != nullptr)
+	{
+		curr = headByIntensity;
+		while (curr != nullptr)
+		{
+			out << &curr->item;
+			curr = curr->nextByIntensity;
+		}
+	}
 }
 
 void List::insert(const Voter& voter)
 {
 	Node *curr;
-	Node *next;
+	Node *prev;
 
-	Node insertNode = new Node (voter);
-	if ((headByName == NULL) && (headByIntensity == NULL))
+	Node insertNode(new Node (voter));
+	if (headByName == NULL)
 	{
 		*headByName = insertNode;
 		*headByIntensity = insertNode;
 	}
-	else if (headByName->nextByName == NULL)
-	{
-		if (0 <= strcmp(insertNode->item.name , cur->item.name))
+	else if (headByName != NULL)
+	{	
+		prev = nullptr;
+		curr = headByName;
+		
+		if (strcmp(voter.getName(), curr->item.getName()) == 0)
 		{
-			insertNode->nextByName = headByName;
+			prev = curr;
+			curr = curr->nextByName;
+			insertNode->nextByName = curr;
+			prev->nextByName = insertNode;
+
+		}
+		else if (strcmp(voter.getName(), curr->item.getName()) < 0)
+		{
+			insertNode->nextByName == curr;
 			headByName = insertNode;
 		}
-		else
+		else if(strcmp(voter.getName(), curr->item.getName()) > 0)
 		{
-			headByName->nextByName = insertNode;
+			while(strcmp(voter.getName(), curr->item.getName()) > 0)
+			{
+				prev = curr;
+				curr = curr->nextByName;
+
+				if(strcmp(voter.getName(), curr->item.getName()) <= 0)
+				{
+					insertNode->nextByName = curr;
+					prev->nextByName = insertNode;
+					break;
+				}
+			}
 		}
+	}
+	
+
+	prev = nullptr;
+	curr = headByIntensity;
+
+	if(voter.getIntensity() > curr->item.getIntensity())
+	{
+		insertNode->nextByIntensity = curr;
+		headByIntensity = insertNode;
+	}
 	else
 	{
-		cur = headByName;
-		next = headByName->nextByName;
-		while ((0 <= strcmp(insertNode->item.name , curr->item.name)) && (0 <= strcmp(insertNode->item.name , next->item.name)) && (nextByName!=NULL))
+		while(curr != nullptr)
 		{
-			cur = next;
-			next = nextByName;
+			prev = curr;
+			curr = curr->nextByIntensity;
+			if(voter.getIntensity < prev->item.getIntensity() && curr == nullptr)
+			{
+				insertNode->nextByIntensity == nullptr;
+				prev->nextByIntensity = insertNode;
+			}
+			else if (voter.getIntensity() < prev->item.getIntensity() && voter.getIntensity > curr->item.getIntensity())
+			{
+				insertNode->nextByIntensity = curr;
+				prev->nextByIntensity = insertNode;
+			}
 		}
-		cur->nextByName = insertNode;
-		insertNode->nextByName = next;
-
-	}	
-
-			
 	}
-
 
 }
 
 Voter* const List::find(const char * const name) const
 {
 	// your code here, return the appropriate value
-	
+	Node * curr(nullptr);
+
+	if(headByName == nullptr)
+	{
+		return nullptr;
+	}
+	else if (headByName != nullptr)
+	{
+		curr = headByName;
+		while(strcmp(curr->item.getName(), name) != 0)
+		{
+			curr = curr->nextByName;
+			if(curr == nullptr)
+			{
+				return nullptr;
+			}
+		}
+		if(strcmp(curr->item.getName(), name) == 0)
+		{
+			return &curr->item;
+		}
+	}
 
 	return nullptr;
 }
@@ -98,8 +186,31 @@ Voter* const List::find(const char * const name) const
 bool List::remove(const char * const name)
 {
 	// your code here, return the appropriate value
-	
+	Node * currName{nullptr};
+	Node * prev{nullptr};
+	Node * currIten{nullptr};
 
+	currName = headByName;
+	currIten = headByIntensity;
+	while (currName->nextByName != nullptr)
+	{
+		prev = currName;
+		currName = currName->nextByName;
+		if(strcmp(name, currName->item.getName()) == 0)
+		{
+			prev->nextByName = currName->nextByName;
+			while(currIten->nextByIntensity != nullptr)
+			{
+				prev = currIten;
+				currIten = currIten->nextByIntensity;
+				if(currIten == currName)
+				{
+					prev->nextByIntensity = currIten->nextByIntensity;
+					delete currName;
+				}
+			}
+		}
+	}
 
 	return false;
 }
