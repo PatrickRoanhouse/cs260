4ba7c5a7918d6942eaafe4c1acd962df754d6a90
PatrickRoanhouse <Patrick.M.Roanhouse@gmail.com>
2016-10-19 00:17:40 -0700

    step 25 fail win

--- asgmts/asgmt01/list.cpp.bak.cpp
+++ asgmts/asgmt01/list.cpp.bak.cpp
@@ -3,52 +3,59 @@
 
 using namespace std;
 
-const char	List::YOUR_NAME[]{"Bowen Johnson"};
+const char	List::YOUR_NAME[]{"Patrick Roanhouse"};
 
 List::List()
 {
 	// your code here, or in this constructor's initialization list
 	headByName = NULL;
 	headByIntensity = NULL;
+
+	
 }
 
 List::~List()
 {
-
 	// your code here
-	Node * curr(nullptr);
-	//Node * prev(nullptr);
-	while (curr != nullptr)
-	{
-		curr = curr->nextByName;
-		delete curr;
-	}
+		
+
+		Node * curr(nullptr);
+		Node * prev(nullptr);
+		curr = headByName;
+		while(curr != nullptr)
+		{
+			curr = curr->nextByName;
+			delete curr;
+		
+		}
+
+
 
 
 }
 
-
-//You will need to uncomment this constructor and write the code for it.
+/*You will need to uncomment this constructor and write the code for it.*/
 
 List::Node::Node(const Voter& voter) : item{voter.getName(), voter.getParty(), voter.getIntensity()}
 	// your initialization list here
 {
 	// your code here
-	nextByName = nullptr;
-	nextByIntensity = nullptr;
+	nextByName(nullptr);
+	nextByIntensity(nullptr);
+
 }
 
 
 void List::displayByName(ostream& out) const
 {
 	// your code here
-	Node * curr{nullptr};
+	node * curr{nullptr};
 
 	Voter::displayColumnHeadings(out);
 
 	if(headByName == nullptr)
 	{
-		out << "List is empty." << endl;
+		out << "List is empty" << endl;
 	}
 	else if(headByName != nullptr)
 	{
@@ -59,55 +66,52 @@ void List::displayByName(ostream& out) const
 			curr = curr->nextByName;
 		}
 	}
+
 }
 
 void List::displayByIntensity(ostream& out) const
 {
 	// your code here
-	Node * curr{nullptr};
-	Voter::displayColumnHeadings(out);
+	Node * curr{nullptr}
+	Voter::displayColumnheadings(out);
 	if(headByIntensity != nullptr)
 	{
 		curr = headByIntensity;
-		while(curr != nullptr)
+		while (curr != nullptr)
 		{
 			out << &curr->item;
 			curr = curr->nextByIntensity;
 		}
-
 	}
 }
 
 void List::insert(const Voter& voter)
 {
+	Node *curr;
+	Node *prev;
 
-
-	// your code here
-	Node * curr;
-	Node * prev;
-	Node * newNode(new Node (voter));
-	//Node * newNode;
-
+	Node newNode(new Node (voter));
 	if (headByName == NULL)
 	{
-		headByName = newNode;
-		headByIntensity = newNode;
+		*headByName = newNode;
+		*headByIntensity = newNode;
 	}
-	else if(headByName != NULL)
-	{
+	else if (headByName != NULL)
+	{	
 		prev = nullptr;
 		curr = headByName;
-
+		
 		if (strcmp(voter.getName(), curr->item.getName()) == 0)
 		{
 			prev = curr;
-			curr = curr ->nextByName;
+			curr = curr->nextByName;
 			newNode->nextByName = curr;
 			prev->nextByName = newNode;
+
 		}
-		else if(strcmp(voter.getName(), curr->item.getName()) < 0)
+		else if (strcmp(voter.getName(), curr->item.getName()) < 0)
 		{
-			newNode->nextByName = curr;
+			newNode->nextByName == curr;
 			headByName = newNode;
 		}
 		else if(strcmp(voter.getName(), curr->item.getName()) > 0)
@@ -126,6 +130,7 @@ void List::insert(const Voter& voter)
 			}
 		}
 	}
+	
 
 	prev = nullptr;
 	curr = headByIntensity;
@@ -141,20 +146,18 @@ void List::insert(const Voter& voter)
 		{
 			prev = curr;
 			curr = curr->nextByIntensity;
-			
 			if(voter.getIntensity() < prev->item.getIntensity() && curr == nullptr)
 			{
-				newNode->nextByIntensity = nullptr;
+				newNode->nextByIntensity == nullptr;
 				prev->nextByIntensity = newNode;
 			}
-			else if(voter.getIntensity() < prev->item.getIntensity() && voter.getIntensity() > curr->item.getIntensity())
+			else if (voter.getIntensity() < prev->item.getIntensity() && voter.getIntensity() > curr->item.getIntensity())
 			{
 				newNode->nextByIntensity = curr;
 				prev->nextByIntensity = newNode;
 			}
 		}
 	}
-
 
 }
 
@@ -167,7 +170,7 @@ Voter* const List::find(const char * const name) const
 	{
 		return nullptr;
 	}
-	else if(headByName != nullptr)
+	else if (headByName != nullptr)
 	{
 		curr = headByName;
 		while(strcmp(curr->item.getName(), name) != 0)
@@ -192,31 +195,29 @@ bool List::remove(const char * const name)
 	// your code here, return the appropriate value
 	Node * currName{nullptr};
 	Node * prev{nullptr};
-	Node * currInten{nullptr};
+	Node * currIten{nullptr};
 
 	currName = headByName;
-	currInten = headByIntensity;
-
-	while(currName->nextByName != nullptr)
+	currIten = headByIntensity;
+	while (currName->nextByName != nullptr)
 	{
 		prev = currName;
 		currName = currName->nextByName;
 		if(strcmp(name, currName->item.getName()) == 0)
 		{
 			prev->nextByName = currName->nextByName;
-			while(currInten->nextByIntensity != nullptr)
+			while(currIten->nextByIntensity != nullptr)
 			{
-				prev = currInten;
-				currInten = currInten->nextByIntensity;
-				if(currInten == currName)
+				prev = currIten;
+				currIten = currIten->nextByIntensity;
+				if(currIten == currName)
 				{
-					prev->nextByIntensity = currInten->nextByIntensity;
+					prev->nextByIntensity = currIten->nextByIntensity;
 					delete currName;
 				}
 			}
 		}
 	}
 
-	
 	return false;
 }
--- asgmts/asgmt01/voter.cpp.bak.cpp
+++ asgmts/asgmt01/voter.cpp.bak.cpp
@@ -1,13 +1,17 @@
-#include <iomanip>
 #include <iostream>
 #include <string.h>
+#include <iomanip>
 #include "voter.h"
 
 using namespace std;
 
-Voter::Voter(const char * const name, const Party party, const int intensity) : party{party}, intensity{intensity}
+
+Voter::Voter(const char * const name, const Party party, const int intensity) :
+	party{party},
+	intensity{intensity}
 {
 	// your code here, or in this constructor's initialization list
+
 
 	this->name = new char[strlen(name)+1];
 	strcpy(this->name, name);
@@ -16,10 +20,10 @@ Voter::Voter(const char * const name, const Party party, const int intensity) : 
 
 Voter::~Voter()
 {
-
 	// your code here
 	delete [] name;
 	name = nullptr;
+
 
 }
 
@@ -28,19 +32,21 @@ void Voter::displayColumnHeadings(ostream& out)
 	// your code here
 	cout << setw(26) << left << "Name" << setw(17) << "Party" << setw(9) << "Intensity" << endl;
 	cout << setw(26) << left << "----" << setw(17) << "-----" << setw(9) << "---------" << endl;
-}
 
+
+}
 ostream& operator<<(ostream& out, Voter* voter)
 {
-	// your code here
-
 	out << setw(26) << left << voter->name << setw(17) << voter->party << setw(9) << right << voter->intensity << endl;
 
 	return out;
+
 }
+
 
 ostream& operator<<(ostream& out, Party party)
 {
+	// your code here
 	switch(party)
 	{
 		case Party::Republican:
@@ -56,8 +62,10 @@ ostream& operator<<(ostream& out, Party party)
 			out << "Green";
 			break;
 		default:
-			out << "Not Recognized";
+			out << "Not Recognized";		
+
 	}
+	
 
 	return out;
 }
